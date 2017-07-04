801e72dcc05503bb60699384aa1e174b462627c7
Esteban Rojas <estered9112@gmail.com>
2017-07-04 10:20:26 -0400

    added .env to save variables

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 "use strict"
 
 var express = require("express");
@@ -10,76 +12,71 @@ var router = express.Router();
 
 var port = process.env.API_PORT || 8080;
 
-mongoose.connect('mongodb://admin:admin@ds119380.mlab.com:19380/reactmovieapp');
+mongoose.connect(process.env.DATABASE_URL);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
-    res.setHeader("Cache-Control", "no-cache");
-    next();
+  res.setHeader("Cache-Control", "no-cache");
+  next();
 });
 
-router.get("/", function (req, res) {
-    res.json({ message: "API Initialized!" });
+router.get("/", function(req, res) {
+  res.json({message: "API Initialized!"});
 });
 
-router.route("/userwatched")
+router.route("/userwatched").get(function(req, res) {
 
-    .get(function (req, res) {
+  UserWatched.find(function(err, userwatched) {
 
-        UserWatched.find(function (err, userwatched) {
+    if (err)
+      res.send(err);
+    res.json(userwatched)
+  });
+}).post(function(req, res) {
+  var userwatched = new UserWatched();
 
-            if (err)
-                res.send(err);
-            res.json(userwatched)
-        });
-    })
+  userwatched.userID = req.body.userID;
+  userwatched.movieID = req.body.movieID;
+  userwatched.addedOrder = req.body.addedOrder;
+  userwatched.save(function(err) {
+    if (err)
+      res.send(err);
+    res.json({message: "UserWatched successfully added!"});
+  });
+});
 
-    .post(function (req, res) {
-        var userwatched = new UserWatched();
+router.route("/userwatched/:userID").get(function(req, res) {
 
-        userwatched.userID = req.body.userID;
-        userwatched.movieID = req.body.movieID;
-        userwatched.addedOrder = req.body.addedOrder;
-        userwatched.save(function (err) {
-            if (err)
-                res.send(err);
-            res.json({ message: "UserWatched successfully added!" });
-        });
-    });
+  UserWatched.find(req.query, function(err, userwatched) {
 
-router.route("/userwatched/:userID")
+    if (err) {
+      res.send(err);
+    }
+    res.json(userwatched)
+  });
 
-    .get(function (req, res) {
+})
 
-        UserWatched.find(req.query, function (err, userwatched) {
-
-            if (err) {
-                res.send(err);
-            }
-            res.json(userwatched)
-        });
-
-    })
-
-router.route("/userwatched")
-
-    .delete(function (req, res) {
-        UserWatched.findOneAndRemove({ movieID: req.query.movieID, userID: req.query.userID, }, function (err, userwatched) {
-            if (err)
-                res.send(err);
-            res.json({ message: "userwatched has been deleted", movie: req.query.movieID, user: req.query.userID })
-        })
-    });
+router.route("/userwatched").delete(function(req, res) {
+  UserWatched.findOneAndRemove({
+    movieID: req.query.movieID,
+    userID: req.query.userID
+  }, function(err, userwatched) {
+    if (err)
+      res.send(err);
+    res.json({message: "userwatched has been deleted", movie: req.query.movieID, user: req.query.userID})
+  })
+});
 
 app.use("/api", router);
 
-app.listen(port, function () {
-    console.log(`api running on port ${port}`);
+app.listen(port, function() {
+  console.log(`api running on port ${port}`);
 });
